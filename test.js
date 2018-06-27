7448bc2e21e4d036276ac6a4477d2c35730cad86
jtenner <tenner.joshua@gmail.com>
2018-06-27 15:47:26 -0400

    First commit!

--- test.js
+++ test.js
@@ -1,19 +1,8 @@
 const grammar = require('./grammar.js');
 const nearley = require('nearley');
 const parser = new nearley.Parser(nearley.Grammar.fromCompiled(grammar));
-
-parser.feed(`
-script test (){
-  show Aya invisible, stageLeft;
-  Aya says \`hello world!\`;
-  move Aya center, visible, quadInOut;
-  addsay \`This is a test.\`;
-  
-}
-`)
-
-console.log(parser.results);
-
 const fs = require("fs");
+
+parser.feed(fs.readFileSync("./input.prjs", 'utf-8'));
 
 fs.writeFileSync("results.json", JSON.stringify(parser.results[0], null, 2), 'utf-8');
