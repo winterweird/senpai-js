1fc70708e370c0e47fb22c82042823637c63c803
jtenner <tenner.joshua@gmail.com>
2018-06-29 16:41:04 -0400

    Lots of development work

--- src/view/Sprite.ts
+++ src/view/Sprite.ts
@@ -1,4 +1,4 @@
-import { IInteractionPoint, IKeyState } from "./util";
+import { IInteractionPoint, IKeyState, ISize } from "./util";
 import { easeLinear } from "../ease";
 import { EventEmitter } from "events";
 import { Identity } from "./Matrix";
@@ -21,6 +21,7 @@ export interface ISprite {
   hover: boolean; //controlled by the stage
   clicked: boolean; //controlled by the stage
   down: boolean; //controlled by the stage
+  up: boolean; //controlled by the stage
   update(): void;
   render(ctx: CanvasRenderingContext2D): void;
 }
@@ -29,19 +30,23 @@ export interface ISpriteProps {
   position: Float64Array | number[];
 }
 
-export class Sprite extends EventEmitter implements ISprite {
+export class Sprite extends EventEmitter implements ISprite, ISize {
   position: Float64Array = new Float64Array(8);
   previousPosition: Float64Array = new Float64Array(8);
   interpolatedPosition: Float64Array = new Float64Array(7);
   animationStart: number = 0;
   animationEnd: number = 0;
   ease: Function = easeLinear;
-  cursor: "pointer" | "default" = "default";
+  cursor: ("pointer" | "default") = "default";
   animationLength: 400;
   active: boolean = false;
   hover: boolean = false;
   clicked: boolean = false;
   down: boolean = false;
+  up: boolean = false;
+  //ISize
+  width: number = 0;
+  height: number = 0;
 
   constructor(props: ISpriteProps) {
     super();
@@ -60,9 +65,18 @@ export class Sprite extends EventEmitter implements ISprite {
 
   }
   update(): void {
-
+    if (this.clicked) {
+      super.emit("click", this);
+    }
+    if (this.down) {
+      super.emit("down", this);
+    }
+    if (this.up) {
+      super.emit("up", this);
+    }
   }
   render(ctx: CanvasRenderingContext2D) {
 
   }
-}
+};
+
--- src/view/util.ts
+++ src/view/util.ts
@@ -1,31 +1,70 @@
+
+import { Character, ICharacter } from "./Character";
+import { Identity } from "./Matrix";
+
 export interface IPoint {
   x: number;
   y: number;
-}
+};
 
 export interface ISize {
   width: number;
   height: number;
-}
+};
+
+export interface ISpriteSheetSize {
+  w: number;
+  h: number;
+};
+
+export interface ISpriteSheetMeta {
+  app: string;
+  version: string;
+  image: string;
+  size: ISpriteSheetSize;
+  scale: number;
+};
+
+export interface ISpriteSheetPoint {
+  x: number;
+  y: number;
+};
+
+export interface ISpriteSheetFrame {
+  frame: ISpriteSheetPoint & ISpriteSheetSize;
+  rotated: boolean;
+  trimmed: boolean;
+  spriteSourceSize: ISpriteSheetPoint & ISpriteSheetSize;
+  sourceSize: ISpriteSheetSize;
+};
+
+export interface ISpriteSheetFrameMap {
+  [frame: string]: ISpriteSheetFrame;
+};
+
+export interface ISpriteSheet {
+  frames: ISpriteSheetFrameMap;
+  meta: ISpriteSheetMeta;
+};
 
 export interface IInteractionPoint extends IPoint {
   id: string;
   type: "Touch" | "Mouse";
   down: boolean;
-}
+};
 
 export interface IKeyState {
   key: string;
   down: boolean;
-}
+};
 
 export interface IKeyData {
   getKey(key: string): IKeyState;
-}
+};
 
 export interface IKeyDataIndex {
   [key: string]: IKeyState;
-}
+};
 
 export class KeyState implements IKeyState {
   constructor(key: string) {
@@ -33,7 +72,7 @@ export class KeyState implements IKeyState {
   }
   key: string = "";
   down: boolean = false;
-}
+};
 
 export class KeyData implements IKeyData {
   constructor() { }
@@ -44,4 +83,43 @@ export class KeyData implements IKeyData {
     }
     return this.keys[key];
   }
-}
+};
+
+export interface ITextureMap {
+  [texture: string]: ImageBitmap;
+};
+
+export async function loadCharacter(name: string): ICharacter {
+  const img = loadImage(`./assets/characters/${name}/spritesheet.png`);
+  const definition: ISpriteSheet = require(`../../assets/characters/${name}/index.json`);
+  const moods: ITextureMap = {};
+  await Promise.all(
+    Object.entries(definition.frames).map(async function([mood, moodDefintion], i) {
+      moods[mood] = await createImageBitmap(
+        await img,
+        moodDefintion.frame.x,
+        moodDefintion.frame.y,
+        moodDefintion.frame.w,
+        moodDefintion.frame.h
+      );
+    })
+  );
+  const character = new Character({
+    name,
+    moods,
+    position: Identity,
+  });
+  return character;
+};
+
+export async function loadImage(src: string): Promise<ImageBitmap> {
+  const res = await fetch(src);
+  const blob = await res.blob();
+  const bmp = await createImageBitmap(blob);
+  return bmp;
+};
+
+export async function loadSoundBuffer(source: Promise<Response>, start: number, finish: number) {
+  const res = await source;
+  const buffer = await res.arrayBuffer();
+};
