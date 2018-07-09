5fb89fbe64038c3af4312524f64bdc1cee97f5ef
jtenner <tenner.joshua@gmail.com>
2018-07-09 12:38:12 -0400

    Add events, stage manager, fix skip animation

--- src/browser/index.ts
+++ src/browser/index.ts
@@ -1,9 +1,6 @@
 import { StageManager, IStageManagerProps } from "./StageManager";
 
 import config from "../../application.config";
-import { IWorkerEvent } from "../events/IWorkerEvent";
-import { IBatchEvent } from "../events/IBatchEvent";
-import { ICreateCharacterEvent } from "../events/ICreateCharacterEvent";
 
 const props: IStageManagerProps = {
   audioContext: new AudioContext,
@@ -15,21 +12,11 @@ const props: IStageManagerProps = {
 const sm = new StageManager(props);
 const worker = new Worker("../worker/index.ts");
 
-async function handleEvent(event:  IWorkerEvent) {
-  const { type } = event;
-
-  if (type === "batch") {
-    const { props } = event as IBatchEvent;
-    for (let i = 0; i < props.events.length; i++) {
-      await handleEvent(props.events[i]);
-    }
-    return;
-  }
-
-
-}
-
-worker.addEventListener("message", e => handleEvent(e.data));
+worker.addEventListener("message",
+  e => sm.handle(e.data)
+    .then(callback => console.log("handled", e.data))
+    .catch(error => console.log("Error handling", e.data, error))
+);
 
 function frame() {
   requestAnimationFrame(frame);
